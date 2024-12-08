edbd122f24810b0c7b122733d54b3d1b6b46ef93
botinplace <botinplace@mail.ru>
2024-12-08 12:18:02 +0300

    fix Application6

--- Core/Config/Config.php
+++ Core/Config/Config.php
@@ -6,7 +6,7 @@ class Config{
         if (file_exists($configFilePath)) {
             require $configFilePath;
         } else {            
-            $self::defineDefaultConstants();
+            self::defineDefaultConstants();
             //error_log("Создайте файл конфигурации: " . $configFilePath . ' на базе шаблона config.simple.php ');
 			//throw new \Exception('Create config.php based on config.sample.php');
 			die('Create config.php based on config.sample.php');
