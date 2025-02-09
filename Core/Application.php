98c45f3748281d541cf3da64a92f0826ac677c7d
botinplace <49042010+botinplace@users.noreply.github.com>
2025-02-09 18:12:29 +0300

    Update Application.php

--- Core/Application.php
+++ Core/Application.php
@@ -5,34 +5,33 @@ use Core\Config\Config;
 use Core\Request;
 use Core\Router;
 
-if (file_exists($_SERVER['SCRIPT_FILENAME']) && pathinfo($_SERVER['SCRIPT_FILENAME'], PATHINFO_EXTENSION) !== 'php') {
+if (
+    file_exists($_SERVER["SCRIPT_FILENAME"]) &&
+    pathinfo($_SERVER["SCRIPT_FILENAME"], PATHINFO_EXTENSION) !== "php"
+) {
     return;
 }
 
-class Application {
+class Application
+{
     private $config;
-    public $pageData = ['title' => 'My Application', 'description' => 'This is a sample app.'];
+    public $pageData = [
+        "title" => "My Application",
+        "description" => "This is a sample app.",
+    ];
     public $routes;
-    private $router;    
+    private $router;
 
-    public function __construct() {
-        // Загрузка конфигурации
-        $this->config = Config::loadConfig(APP . 'Config/config.php');
-        
-        // Загрузка маршрутов из файла
-        $this->routes = $this->loadFile(APP . 'routes.php');
-        
-        // Загрузка роутера
-        $this->router = new Router($this);
+    public function __construct()
+    {
+        $this->config = Config::loadConfig(APP . "Config/config.php");
+        $this->router = new Router();
         $this->dispatchCurrentRoute();
     }
 
-    public function loadFile(string $filePath) {
-        return file_exists($filePath) ? require $filePath : [];
-    }
-
-    private function dispatchCurrentRoute() {
-        $path = $this->sanitizePath($_SERVER['REQUEST_URI']);
+    private function dispatchCurrentRoute()
+    {
+        $path = $this->sanitizePath($_SERVER["REQUEST_URI"]);
         try {
             $this->router->dispatch($path);
         } catch (\Exception $e) {
@@ -40,21 +39,40 @@ class Application {
         }
     }
 
-    private function sanitizePath($uri) {
+    private function sanitizePath($uri)
+    {
         $path = filter_var(trim($uri), FILTER_SANITIZE_URL);
-        $path = explode('?', $path, 2)[0];
+        $path = explode("?", $path, 2)[0];
 
         // Удаление лишнего (например /admin/)
-        $path = (URI_FIXER !== '') ? preg_replace('/^' . preg_quote(URI_FIXER, '/') . '(\/|$)/', '/', $path) : $path;
-        $path = (BASE_URL !== '') ? preg_replace('/^\/' . preg_quote(BASE_URL, '/') . '(\/|$)/', '/', $path) : $path;
+        $path =
+            URI_FIXER !== ""
+                ? preg_replace(
+                    "/^" . preg_quote(URI_FIXER, "/") . '(\/|$)/',
+                    "/",
+                    $path
+                )
+                : $path;
+        $path =
+            BASE_URL !== ""
+                ? preg_replace(
+                    "/^\/" . preg_quote(BASE_URL, "/") . '(\/|$)/',
+                    "/",
+                    $path
+                )
+                : $path;
 
-        return $path ?: '/';
+        return $path ?: "/";
     }
 
-    private function handleError(\Exception $e) {
+    private function handleError(\Exception $e)
+    {
         http_response_code(500);
-        echo 'Ошибка: ' . htmlspecialchars($e->getMessage(), ENT_QUOTES, 'UTF-8');
+        echo "Ошибка: " .
+            htmlspecialchars($e->getMessage(), ENT_QUOTES, "UTF-8");
     }
 
-    public function run() {}
+    public function run()
+    {
+    }
 }
