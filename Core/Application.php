9b9490229b3d84f75e49fdf0682874afc4e78595
botinplace <49042010+botinplace@users.noreply.github.com>
2025-02-09 14:12:52 +0300

    Update Application.php

--- Core/Application.php
+++ Core/Application.php
@@ -11,45 +11,50 @@ if (file_exists($_SERVER['SCRIPT_FILENAME']) && pathinfo($_SERVER['SCRIPT_FILENA
 
 class Application {
     private $config;
-	public  $pageData = ['title' => 'My Application', 'description' => 'This is a sample app.'];
-	public $routes;
+    public $pageData = ['title' => 'My Application', 'description' => 'This is a sample app.'];
+    public $routes;
     private $router;    
+
     public function __construct() {
-		// Загрузка конфигурации
+        // Загрузка конфигурации
         $this->config = Config::loadConfig(APP . 'Config/config.php');
-		
-		// Загрузка маршрутов из файла
+        
+        // Загрузка маршрутов из файла
         $this->routes = $this->loadFile(APP . 'routes.php');
-		
- 		// Загрузка роутера
-		$this->router = new Router($this);
-		$this->dispatchCurrentRoute();
+        
+        // Загрузка роутера
+        $this->router = new Router($this);
+        $this->dispatchCurrentRoute();
     }
-	
-	public function loadFile(string $filePath){
-		return file_exists($filePath) ? require $filePath : ([]);
-	}
+
+    public function loadFile(string $filePath) {
+        return file_exists($filePath) ? require $filePath : [];
+    }
 
     private function dispatchCurrentRoute() {
-        // Получение и очистка текущего пути
-        $path = filter_var(trim($_SERVER['REQUEST_URI']), FILTER_SANITIZE_URL);
+        $path = $this->sanitizePath($_SERVER['REQUEST_URI']);
+        try {
+            $this->router->dispatch($path);
+        } catch (\Exception $e) {
+            $this->handleError($e);
+        }
+    }
+
+    private function sanitizePath($uri) {
+        $path = filter_var(trim($uri), FILTER_SANITIZE_URL);
         $path = explode('?', $path, 2)[0];
 
         // Удаление лишнего (например /admin/)
         $path = (URI_FIXER !== '') ? preg_replace('/^' . preg_quote(URI_FIXER, '/') . '(\/|$)/', '/', $path) : $path;
         $path = (BASE_URL !== '') ? preg_replace('/^\/' . preg_quote(BASE_URL, '/') . '(\/|$)/', '/', $path) : $path;
-        $path = $path ?: '/';
 
-        // Запуск маршрутизации
-        try {
-            $this->router->dispatch($path);
-        } catch (Exception $e) {
-			http_response_code(500);
-            echo 'Ошибка: ' . $e->getMessage();			
-        }
+        return $path ?: '/';
     }
 
-    public function run() {
-		
-	}
+    private function handleError(\Exception $e) {
+        http_response_code(500);
+        echo 'Ошибка: ' . htmlspecialchars($e->getMessage(), ENT_QUOTES, 'UTF-8');
+    }
+
+    public function run() {}
 }
