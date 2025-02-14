8020d157845cbb5a34ce8cf5c18135c2e1e6178b
botinplace <49042010+botinplace@users.noreply.github.com>
2025-02-14 14:56:31 +0300

    Create ErrorHandler.php

---
+++ Core/ErrorHandler.php
@@ -0,0 +1,27 @@
+namespace Core;
+
+class ErrorHandler {
+    private $logFile = 'log';
+    private $maxLines = 100;
+
+    public function __construct() {
+        set_error_handler([$this, 'customErrorHandler']);
+    }
+    public function customErrorHandler($errno, $errstr, $errfile, $errline) {
+        $errorCount = 0;
+        if (file_exists($this->logFile)) {
+            $errorCount = count(file($this->logFile));
+        }
+        if ($errorCount >= $this->maxLines) {
+            $logNum = 1;
+
+            while (file_exists($this->logFile . $logNum)) {
+                $logNum++;
+            }
+
+            rename($this->logFile, $this->logFile . $logNum);
+        }
+
+        error_log("[$errno] $errstr in $errfile on line $errline\n", 3, $this->logFile);
+    }
+}
