c944742890fe8415085c164db4c12257006bddbe
botinplace <49042010+botinplace@users.noreply.github.com>
2025-02-18 09:55:30 +0300

    Update and rename Core/DB/ClassNoDB/pdoNoDBClass.php to Core/Database/ClassNoDB.php

---
+++ Core/Database/ClassNoDB.php
@@ -0,0 +1,81 @@
+<?php
+class ClassNoDB
+{
+    /**
+     * Объект PDO (заглушка).
+     */
+    public static $dbh = null;
+
+    /**
+     * Statement Handle (заглушка).
+     */
+    public static $sth = null;
+
+    /**
+     * Выполняемый SQL запрос (заглушка).
+     */
+    public static $query = '';
+
+    /**
+     * Возвращает true, чтобы имитировать успешное выполнение в случае добавления.
+     */
+    public static function add($query, $param = array())
+    {
+        self::$query = $query;
+        // Вернуть фиктивный ID, если запрос выполнен (симуляция успешного выполнения)
+        return 1; // Имитируем успешное добавление, возвращая ID = 1
+    }
+
+    /**
+     * Имитирует выполнение запроса.
+     */
+    public static function set($query, $param = array())
+    {
+        self::$query = $query;
+        // Имитация успешного выполнения запроса
+        return true; // Успешно
+    }
+
+    /**
+     * Имитирует получение одной строки из таблицы.
+     */
+    public static function getRow($query, $param = array())
+    {
+        self::$query = $query;
+        // Возвращаем фиктивные данные
+        return ['id' => 1, 'name' => 'Test']; // Пример возвращаемого результата
+    }
+
+    /**
+     * Имитирует получение всех строк из таблицы.
+     */
+    public static function getAll($query, $param = array())
+    {
+        self::$query = $query;
+        // Возвращаем фиктивные данные
+        return [
+            ['id' => 1, 'name' => 'Test 1'],
+            ['id' => 2, 'name' => 'Test 2'],
+        ]; // Пример возвращаемого результата
+    }
+
+    /**
+     * Имитирует получение значения.
+     */
+    public static function getValue($query, $param = array(), $default = null)
+    {
+        self::$query = $query;
+        // Возвращаем имитацию значения
+        return 'Test Value'; // Пример возвращаемого результата
+    }
+
+    /**
+     * Имитирует получение столбца таблицы.
+     */
+    public static function getColumn($query, $param = array())
+    {
+        self::$query = $query;
+        // Возвращаем фиктивные данные
+        return ['Value 1', 'Value 2']; // Пример возвращаемого результата
+    }
+}
