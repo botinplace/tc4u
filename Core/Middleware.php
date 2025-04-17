ed97fb2815fc9b2b7ffc588c4c17121cbe9f9d14
botinplace <49042010+botinplace@users.noreply.github.com>
2025-04-17 12:44:06 +0300

    Update Middleware.php

--- Core/Middleware.php
+++ Core/Middleware.php
@@ -5,5 +5,5 @@ namespace Core;
 use Core\Request;
 
 abstract class Middleware{
-    abstract public function handle(Request $request, callable $next);
+    abstract public function handle(array $params, array $pagedata);
 }
