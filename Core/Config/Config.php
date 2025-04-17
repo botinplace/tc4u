df355f7636f2dbef056908b142f7525d56083cb6
botinplace <49042010+botinplace@users.noreply.github.com>
2025-04-17 10:59:30 +0300

    Update Config.php

--- Core/Config/Config.php
+++ Core/Config/Config.php
@@ -1,24 +1,29 @@
 <?php
 namespace Core\Config;
 
+//use Core\Config\LoadEnv;
+
 class Config {
     private static $config = [];
 
     public static function load(): void {
+        
+       //LoadEnv::load(__DIR__ . '/.env');
+
         // Основной файл
         self::$config = require CONFIG_DIR . 'config.php';
-		if (!self::$config) {
-			throw new \Exception("Config not loaded properly");
-		}
+        if (!self::$config) {
+            throw new \Exception("Config not loaded properly");
+        }
 
         // Локальные переопределения
         if (file_exists(CONFIG_DIR . 'config.local.php')) {
             self::$config = array_merge(self::$config, require CONFIG_DIR . 'config.local.php');
         }
 
-	    $fixed_url = !empty( self::get('uri_fixer') ) ? self::get('uri_fixer').(!empty(BASE_URL) ? '/' : '') : '').BASE_URL;
-	    self::$config = array_merge(self::$config,  [ 'app' => [ 'fixed_url' => $fixed_url ] ] );	   
-    
+        $fixed_url = !empty(self::get('uri_fixer')) ? self::get('uri_fixer').(!empty(BASE_URL) ? '/' : '') : '').BASE_URL;
+        self::$config = array_merge(self::$config,  ['app' => ['fixed_url' => $fixed_url]]);
+
         self::validateConfig();
     }
 
@@ -34,12 +39,12 @@ class Config {
         return $value;
     }
 
-	private static function validateConfig(): void {
-		$requiredKeys = ['app.uri_fixer'];
-		foreach ($requiredKeys as $key) {
-			if (self::get($key) === null) {
-				throw new \Exception("Missing required config key '{$key}'");
-			}
-		}
-	}
+    private static function validateConfig(): void {
+        $requiredKeys = ['app.uri_fixer'];
+        foreach ($requiredKeys as $key) {
+            if (self::get($key) === null) {
+                throw new \Exception("Missing required config key '{$key}'");
+            }
+        }
+    }
 }
