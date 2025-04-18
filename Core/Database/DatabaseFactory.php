6f474c7817d20816e078e677d7165959ab32e4eb
botinplace <49042010+botinplace@users.noreply.github.com>
2025-04-19 01:46:31 +0300

    Update DatabaseFactory.php

--- Core/Database/DatabaseFactory.php
+++ Core/Database/DatabaseFactory.php
@@ -1,21 +1,19 @@
 <?php
+
 namespace Core\Database;
 
 class DatabaseFactory {
-    public static function create($type) {
-        switch ($type) {
-            case 'pgsql':
-                return new PostgreSQL();
-            case 'mysql':
-                return new MySQL();
-            case 'mssql':
-                return new MsSQL();
-            case 'sqlite':
-                return new SQLite();
-            case 'nodb':
-                return new NoDB();
-            default:
-                throw new \Exception("Неподдерживаемый тип базы данных: $type");
-        }
+    public static function create(string $type): DatabaseInterface {
+        return match(strtolower($type)) {
+            'mysql' => MySQL::getInstance(),
+            'pgsql', 'postgresql' => PostgreSQL::getInstance(),
+            'sqlite' => SQLite::getInstance(),
+            'mssql', 'sqlsrv' => MSSQL::getInstance(),
+            'nodb' => NoDB::getInstance(),
+            default => throw new \InvalidArgumentException(
+                "Неподдерживаемый тип базы данных: {$type}. " .
+                "Поддерживаемые: mysql, pgsql, sqlite, mssql, nodb"
+            )
+        };
     }
 }
