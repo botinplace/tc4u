ec21d075ca31b0e7f541c442db5e0ce93b0e8e7f
botinplace <49042010+botinplace@users.noreply.github.com>
2025-04-23 12:04:07 +0300

    Update Model.php

--- Core/Model.php
+++ Core/Model.php
@@ -5,7 +5,26 @@ use Core\Database\DatabaseFactory;
 
 abstract class Model {
     protected $db;
-    public function __construct($dbType) {
-        $this->db = DatabaseFactory::create($dbType);
+    
+    public function __construct(string $connectionName = null) {
+        $this->db = $this->resolveConnection($connectionName);
+    }
+    
+    protected function resolveConnection(?string $connectionName): DatabaseInterface {
+        try {
+            return DatabaseFactory::create(
+                $connectionName ?? $this->getDefaultConnectionName()
+            );
+        } catch (\RuntimeException $e) {
+            // Попытка использовать подключение по умолчанию
+            if ($connectionName !== null && $connectionName !== 'default') {
+                return DatabaseFactory::create('default');
+            }
+            throw $e;
+        }
+    }
+    
+    protected function getDefaultConnectionName(): string {
+        return 'default';
     }
 }
