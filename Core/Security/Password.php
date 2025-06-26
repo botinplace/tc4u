d4f65dc13e74e3c6d16213b8100403be113a584e
botinplace <49042010+botinplace@users.noreply.github.com>
2025-06-26 23:47:48 +0300

    Create Password.php

---
+++ Core/Security/Password.php
@@ -0,0 +1,15 @@
+<?php
+namespace Core\Security;
+
+class Password
+{
+    public static function hash(string $password): string
+    {
+        return password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]);
+    }
+
+    public static function verify(string $password, string $hash): bool
+    {
+        return password_verify($password, $hash);
+    }
+}
