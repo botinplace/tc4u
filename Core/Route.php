a3dc179d9e8a8a9f893db71279eb50e82a7c31a7
botinplace <49042010+botinplace@users.noreply.github.com>
2025-06-29 10:37:04 +0300

    Update Route.php

--- Core/Route.php
+++ Core/Route.php
@@ -6,19 +6,21 @@ class Route {
     public $path;
     public $controller;
     public $needAuth;
+    public $onlyforguest;
     public $middlewares;
     public $name;
 
-    public function __construct( $method, $path, $controller, $needAuth = false,$name='',$middlewares=[]) {
+    public function __construct( $method, $path, $controller, $needAuth = false,$onlyforguest = false,$name='',$middlewares=[]) {
        $this->method = strtoupper($method);
        $this->path = $this->normalizePath($path);
        $this->controller = $controller;
        $this->needAuth = $needAuth;
+       $this->onlyforguest = $onlyforguest;
        $this->middlewares = $middlewares;
        $this->name = $name;
     }
 	public static function __set_state(array $state) {
-			$obj = new self( $state['method'], $state['path'], $state['controller'], $state['needAuth'],$state['name'],$state['middlewares'] );			
+			$obj = new self( $state['method'], $state['path'], $state['controller'], $state['needAuth'], $state['onlyforguest'],$state['name'],$state['middlewares'] );			
 			return $obj;
 	}
 	
