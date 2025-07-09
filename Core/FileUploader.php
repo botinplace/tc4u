c4f9d82fc09855d38cdbf98f1f7a7ab879ca1bed
botinplace <49042010+botinplace@users.noreply.github.com>
2025-07-09 22:52:21 +0300

    Update FileUploader.php

--- Core/FileUploader.php
+++ Core/FileUploader.php
@@ -14,7 +14,8 @@ class FileUploader
 
     public function __construct(string $baseUploadPath = null)
     {
-        $this->baseUploadPath = $baseUploadPath ?? $_SERVER['DOCUMENT_ROOT'] . '/uploads/';
+        //$this->baseUploadPath = $baseUploadPath ?? $_SERVER['DOCUMENT_ROOT'] . '/uploads/';
+        $this->baseUploadPath = $baseUploadPath ?? ROOT . '/uploads/';
     }
 
     public function setAllowedExtensions(array $extensions): self
